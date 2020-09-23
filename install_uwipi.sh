9ad1b5d966c0dd3b53ddce728899d4b99aeb2411
jonmon6691 <jonmon6691@gmail.com>
2020-09-22 22:35:02 -0700

    fix typo in the board path

--- install_uwipi.sh
+++ install_uwipi.sh
@@ -7,7 +7,7 @@ cat uwipi_buildroot.config >> buildroot/configs/raspberrypi0w_defconfig
 echo "OK"
 
 echo -n "Installing rootfs_overlay... "
-cp -r rootfs_overlay buildroot/boards/raspberrypi0w/
+cp -r rootfs_overlay buildroot/board/raspberrypi0w/
 echo "OK"
 
 
